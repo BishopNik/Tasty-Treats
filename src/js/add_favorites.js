39635c6dc56218d0cb6f302ad9a4fac199d6037c
IrynaRybalka <129180240+IrynaRybalka@users.noreply.github.com>
2023-09-17 00:50:25 +0300

    half

---
+++ src/js/add_favorites.js
@@ -0,0 +1,24 @@
+// get favorites from local storage or empty array
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+// register click event listener
+document.querySelector('.list').addEventListener('click', function(e) {
+  var id = e.target.id,
+      item = e.target,
+      index = favorites.indexOf(id);
+  // return if target doesn't have an id (shouldn't happen)
+
+  // item is not favorite
+  if (index == -1) {
+    favorites.push(id);
+    item.className = 'fav';
+  // item is already favorite
+  } else {
+    favorites.splice(index, 1);
+    item.className = '';
+  }
+  // store array in local storage
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+});
+
+// local storage stores strings so we use JSON to stringify for storage and parse to get out of storage
