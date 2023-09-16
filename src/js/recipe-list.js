043f8ccb9806b456b06694df11438023eb5dd48d
AndriiBilyiV <andrey.dobraf@gmail.com>
2023-09-16 15:27:51 +0300

    add-cards

--- src/js/recipe-list.js
+++ src/js/recipe-list.js
@@ -1,17 +1,71 @@
 
-const page = document.querySelector('.page');
-const totalPages = document.querySelector('.totalPages');
 const rangeButtons = document.querySelector('.range-btns')
-const btn = document.querySelector('.check');
 const forwardButtons = document.querySelectorAll('.forward-arrow-btn-js');
 const backButtons = document.querySelectorAll('.back-arrow-btn-js');
+const recipeCards = document.querySelector('.recipe-cards');
 
-btn.addEventListener('click', (evt) => {
-    evt.preventDefault();
+
+async function renderCards() {
+    let responseData;
+    await fetch('https://tasty-treats-backend.p.goit.global/api/recipes?category=Beef&page=1&limit=9')
+        .then(resp => { return resp.json() })
+        .then(data => {responseData = data})
+    console.log(responseData)
+    const recipesInfo = responseData.results;
+    let htmlCards = '';
+    recipesInfo.forEach(elm => {
+        htmlCards += `<li
+	class="recipe-item mainblock "
+	style="
+		background: linear-gradient(1deg, rgba(5, 5, 5, 0.6) 4.82%, rgba(5, 5, 5, 0) 108.72%),
+			url(${elm.thumb}), lightgray 50%; background-size: cover;
+	"
+>
+        <svg class="like" width="22" height="22">
+		<use href="../img/icon/icon.svg#icon-like-full"></use>
+	</svg>
+	<h3 class="recipe-item-name">${elm.title}</h3>
+	<p class="recipe-item-about">${elm.instructions}</p>
+	<div class="recipe-item-option">
+		<div class="recipe-item-rating">
+			<span class="recipe-item-rating-num">${elm.rating}</span>
+			<ul class="recipe-item-rating-stars">
+				<li class="recipe-item-rating-star">
+					<svg class="stars-full">
+						<use href="../img/icon/icon.svg#icon-star"></use>
+					</svg>
+				</li>
+				<li class="recipe-item-rating-star">
+					<svg class="stars-full">
+						<use href="../img/icon/icon.svg#icon-star"></use>
+					</svg>
+				</li>
+				<li class="recipe-item-rating-star">
+					<svg class="stars-full">
+						<use href="../img/icon/icon.svg#icon-star"></use>
+					</svg>
+				</li>
+				<li class="recipe-item-rating-star">
+					<svg class="stars-full">
+						<use href="../img/icon/icon.svg#icon-star"></use>
+					</svg>
+				</li>
+				<li class="recipe-item-rating-star">
+					<svg class="stars">
+						<use href="../img/icon/icon.svg#icon-star"></use>
+					</svg>
+				</li>
+			</ul>
+		</div>
+		<load ="./button.html" class="recipe-item-see" type="button" name-button="See recipe" />
+	</div>
+    </li>`
+    })
+    recipeCards.innerHTML = htmlCards;
     forwardButtons.forEach((elm) => elm.disabled = false)
     backButtons.forEach((elm) => elm.disabled = false)
-    const pageValue = Number(page.value);
-    const totalValue = Number(totalPages.value);
+    const pageValue = Number(responseData.page);
+    const totalValue = Number(responseData.totalPages);
     let rangeButtonsArray = '';
     if (pageValue === 1) {
         backButtons.forEach((elm) => elm.disabled = true)
@@ -28,6 +82,6 @@ btn.addEventListener('click', (evt) => {
         rangeButtonsArray += `<button class="pagination-btn number-btn">${pageValue + 1}</button><button class="pagination-btn number-btn">...</button>`
     }
     rangeButtons.innerHTML = rangeButtonsArray;
-})
+}
 
-
+renderCards();
