245f816b389cf53259fb431e9942c950c7fe2cec
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-09-16 14:51:38 +0200

    JS, CSS
    Исправила ползунок в мобильной версии, переписала запрос через axios. Написаны функции для выбора текущей категории или всех категорий.

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,33 +1,34 @@
+// Подключаю axios
+import axios from 'axios';
+
+// Забираю из верстки кнопку "All categories" и контейнер для списка категорий
 const list = document.querySelector(".categories-list");
 const allCategoriesBtn = document.querySelector(".all-categories-btn");
 
+let listArr;
 
-const BASE_URL = "https://tasty-treats-backend.p.goit.global/api";
-
-
-function fetchCategories() {
-    return fetch(`${BASE_URL}/categories`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(console.log('Oops! Something went wrong! Try reloading the page!'))
-        }
-        return response.json();
-    })
+// Делаю запрос на получение данных
+async function fetchCategories() {
+    axios.defaults.baseURL = "https://tasty-treats-backend.p.goit.global/api";
+    const response = await axios.get(`/categories`);
+    return response.data;  
 }
 
-
+// Обработка данных, создание списка категорий в HTML
 fetchCategories()
     
 .then(data => {
     let arrCalegories = [...data]
-    list.innerHTML = createCategoriesList(arrCalegories);
-    // return arrCalegories
+    list.insertAdjacentHTML("beforeend", createCategoriesList(arrCalegories));
+    listArr = [...list.children];
 }) 
         
 .catch(() => {
     console.log('Oops! Something went wrong! Try reloading the page!')
 })
 
+
+// Функция для создания списка
 function createCategoriesList(arr) {
     return arr.map(({ name, _id }) => `
     <li><button type="button" class="categories-btn" id="${_id}">${name}</button></li>
@@ -35,15 +36,19 @@ function createCategoriesList(arr) {
 }
 
 
+// Ставлю прослушиватели на кнопку и список
 allCategoriesBtn.addEventListener("click", onAllCategoriesBtnClick);
 list.addEventListener("click", onListClick)
 
+
+// Функция для передачи данных всех категорий
 function onAllCategoriesBtnClick() {
+    clearCurrentCategory(listArr);
     fetchCategories()
     
     .then(data => {
-        console.dir([...data]);
         let arrCalegories = [...data]
+        console.dir(arrCalegories);
         return arrCalegories
     }) 
             
@@ -52,13 +57,25 @@ function onAllCategoriesBtnClick() {
     })
 }
 
+
+// Функция для передачи данных одной категории
 function onListClick(evt) {
-    console.dir(evt.target);
-    // .classList.remove(cls)
+    clearCurrentCategory(listArr);
+    
     evt.target.classList.add("curent-category")
     let currentCategory = evt.target.textContent;
     console.dir(currentCategory);
 
     return currentCategory;
+}
+
+
+// Функция для снятия выделения текущей категории 
+function clearCurrentCategory(arr) {
+    for (let i = 0; i < arr.length; i += 1){
+        if (arr[i].children[0].classList.contains("curent-category")) {
+            arr[i].children[0].classList.remove("curent-category")
+        } 
+    }
 }
 
