ed56d6e425184e0805d8ecfeaa936325abf011ed
VadymVelenets <128249544+VadymVelenets@users.noreply.github.com>
2023-09-16 21:53:02 +0300

    1

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,55 +1,50 @@
-import { finallInitPage } from '../recipe';
-import { fetchPopular } from '../API_requests/fetchPopular';
-const refs = {
-  list: document.querySelector('.popular-list'),
-  list_mobile: document.querySelector('.popular-list.mobile'),
-};
-function renderPopular(arrayResp) {
-  const markup = arrayResp
-    .map(({ title, description, preview, _id }) => {
-      return `       
-         <li class="popular-item" data-id="${_id}">
-      <div class="popular-wraper">
-        <img
-          class="popular-img"
-          src="${preview}"
-          alt="${title}"
-          loading="lazy"
-          height="64"
-          width="64"
-        />
-      </div>
-      <div class="popular-containet-description">
-        <h3 class="popular-title">${title}</h3>
-        <p class="popular-description">${description}</p>
-      </div>
-    </li>`;
-    })
-    .join('');
-  refs.list.insertAdjacentHTML('beforeend', markup);
-  refs.list_mobile.insertAdjacentHTML('beforeend', markup);
+const recipiesEl = document.querySelector('.recipe-list');
+const BASE_URL =
+  'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
+
+// console.log(recipiesEl);
+// console.log(BASE_URL);
+
+async function getRecipies() {
+  try {
+    let response = await fetch(BASE_URL);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    let recipesArr = await response.json();
+    return recipesArr;
+  } catch (error) {
+    console.log(`Failed to fetch recipes: ${error}`);
+  }
 }
 
-async function popularFetchAndRender() {
-  const data = await fetchPopular();
-  renderPopular(data);
+function renderCart(recipesArr) {
+  const markUp = recipesArr
+    .map(({ title, description, preview, _id }) => {
+      return `
+      <li class="popular-item" data-id="${_id}">
+        <div class="popular-wraper">
+          <img
+            class="popular-img"
+            src="${preview}"
+            alt="${title}"
+            loading="lazy"
+            height="64"
+            width="64"
+          />
+        </div>
+        <div class="popular-containet-description">
+          <h3 class="popular-title">${title}</h3>
+          <p class="popular-description">${description}</p>
+        </div>
+      </li>
+    `;
+    })
+    .join('');
+
+  recipiesEl.insertAdjacentHTML('beforeend', markUp);
 }
-popularFetchAndRender();
-refs.list.addEventListener('click', e => {
-  const refLI = e.target.closest('.popular-item');
-  try {
-    let is = refLI.nodeName;
-  } catch (error) {
-    return;
-  }
-  finallInitPage(refLI.dataset.id);
-});
-refs.list_mobile.addEventListener('click', e => {
-  const refLI = e.target.closest('.popular-item');
-  try {
-    let is = refLI.nodeName;
-  } catch (error) {
-    return;
-  }
-  finallInitPage(refLI.dataset.id);
-});
+
+if (recipiesEl) {
+  getRecipies().then(renderCart);
+}
---
+++ src/main.js
@@ -0,0 +1,16 @@
+<<<<<<< Updated upstream
+=======
+/** @format */
+
+import './js/recipe';
+import './js/categories';
+import './js/filters';
+import './js/recipe-list';
+import './js/rating';
+import './js/order';
+import './js/mobile-menu';
+import './js/scrollUp';
+import './js/fetch-popular';
+import './js/popular';
+
+>>>>>>> Stashed changes
