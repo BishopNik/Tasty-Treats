88e83695b4f63b2fe423d090aa3d526c3678e584
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-09-17 14:45:03 +0200

    изменил блок тегов модального окна и убрал бордер кнопки рецепта

--- src/js/rating-markup.js
+++ src/js/rating-markup.js
@@ -3,7 +3,7 @@
 export function ratingRecipe(rating) {
 	let markupRating = ``;
 	for (let index = 1; index <= 5; index++) {
-		if (index <= rating) {
+		if (index <= Math.round(rating)) {
 			markupRating += `<li class="recipe-item-rating-star">
 						<svg class="stars-full">
 							<use href="../img/icon/icon.svg#icon-star"></use>
