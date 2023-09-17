5dbc4a58da7c4a81683668dced5c0826e4a2fd78
vosob <osobskyi.vlad@gmail.com>
2023-09-17 21:33:58 +0300

    upd slider

--- src/js/dish-slider.js
+++ src/js/dish-slider.js
@@ -4,40 +4,27 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 const swiper = new Swiper('.dish-slider', {
-  slidesPerView: 1.85,
-  spaceBetween: 8,
-  loop: false,
-  breakpoints: {
-    375: {
-      slidesPerView: 1.85,
-      spaceBetween: 8,
-      loop: false,
-    },
-    768: {
-      slidesPerView: 2.6,
-      spaceBetween: 16,
-      loop: false,
-    },
-    1280: {
-      slidesPerView: 2.5,
-      spaceBetween: 16,
-      loop: false,
-    },
-  },
+  slidesPerView: 0.8,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+
+  keyboard: {
+    enabled: true,
   },
 });
 
 async function fetchDishes() {
   try {
-    const res = await axios.get('https://tasty-treats-backend.p.goit.global/api/events');
-    
+    const res = await axios.get(
+      'https://tasty-treats-backend.p.goit.global/api/events'
+    );
+
     const markup = createMarkup(res.data);
-    
+
     const mainDiv = document.querySelector('.swiper-wrapper');
-    
+
     mainDiv.innerHTML = markup;
   } catch (err) {
     console.log('Error:', err);
@@ -50,13 +37,15 @@ function createMarkup(arr) {
   return arr
     .map(
       ({ cook: { imgUrl }, topic: { name, area, imgWebpUrl, previewUrl } }) => `
-        <div style="background-image: url(${imgUrl})" class="swiper-slide slider-cook-img"></div>
-        <div class="swiper-slide slider-dish-container">
+      <div class="swiper-slide" style="min-width: 871px; display: flex; flex-direction: row; gap: 16px;">
+        <div style="background-image: url(${imgUrl})" class="slider-cook-img"></div>
+        <div class="slider-dish-container">
           <img src="${previewUrl}">
           <h2 class="">${name}</h2>
           <p>${area}</p>
         </div>
-        <div style="background-image: url(${imgWebpUrl})" class="swiper-slide slider-dish-fullscreen"></div>
+        <div style="background-image: url(${imgWebpUrl});" class="slider-dish-fullscreen"></div>
+      </div>
       `
     )
     .join('');
