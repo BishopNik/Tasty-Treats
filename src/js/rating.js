96bcda1acb0966f6eb49473ea865818ede887dcc
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-09-18 19:43:11 +0200

    add validation and patch requests

--- src/js/rating.js
+++ src/js/rating.js
@@ -1,19 +1,43 @@
+import { updateRating } from './fetch-api';
+
 function toggleModal(refs) {
-    refs.modal.classList.toggle('is-hidden');
-  }
+  refs.modal.classList.toggle('is-hidden');
+}
 
-export function createListeners() {
-
+export function createListeners(id) {
   const refs = {
     openModalBtn: document.querySelector('#modal-rating-opener'),
     closeModalBtn: document.querySelector('#modal-rating-closer'),
     modal: document.querySelector('#modal-rating-js'),
+    submit: document.querySelector('.modal-submit-button-rating'),
+    form: document.querySelector('.rating-form'),
   };
-    
-    
-    console.log(refs)
-    
 
   refs.openModalBtn.addEventListener('click', () => toggleModal(refs));
   refs.closeModalBtn.addEventListener('click', () => toggleModal(refs));
+
+  console.log(refs);
+
+  refs.form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const data = Object.fromEntries(form);
+
+    console.log(Object.fromEntries(form));
+
+    try {
+      const responseData = await updateRating(
+        id,
+        Number(data.rate),
+        data.email
+      );
+      toggleModal(refs);
+    } catch (error) {
+      // notifix;
+    }
+
+    console.log(responseData);
+  });
 }
