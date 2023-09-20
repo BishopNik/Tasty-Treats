862060fece0a524b02088b1302c060e9ddf356c6
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-09-20 18:24:07 +0200

    ошибка закрытия окна заказа

--- src/js/order.js
+++ src/js/order.js
@@ -1,56 +1,53 @@
+/** @format */
+
 import { makeOrder } from './fetch-api';
 import Inputmask from 'inputmask';
 import Notiflix from 'notiflix';
 
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('#modal-order-opener'),
-    openModalBtnCart: document.querySelector('#modal-order-opener-cart'),
-    closeModalBtn: document.querySelector('#modal-order-closer'),
-    modal: document.querySelector('#modal-order-js'),
-    submit: document.querySelector('.modal-submit-button-order'),
-    form: document.querySelector('.modal-order-form'),
-    phone: document.querySelector('#modal-order-tel'),
-  };
+	const refs = {
+		openModalBtn: document.querySelector('#modal-order-opener'),
+		openModalBtnCart: document.querySelector('#modal-order-opener-cart'),
+		closeModalBtn: document.querySelector('#modal-order-closer'),
+		modal: document.querySelector('#modal-order-js'),
+		submit: document.querySelector('.modal-submit-button-order'),
+		form: document.querySelector('.modal-order-form'),
+		phone: document.querySelector('#modal-order-tel'),
+	};
 
-  new Inputmask('+380(99) 999-99-99').mask(refs.phone);
+	new Inputmask('+380(99) 999-99-99').mask(refs.phone);
 
-  refs.openModalBtnCart.addEventListener('click', toggleModal);
-  console.log(refs.openModalBtn);
-  refs.openModalBtn.addEventListener('click', toggleModal);
+	refs.openModalBtnCart.addEventListener('click', toggleModal);
+	if (refs.openModalBtn) {
+		refs.openModalBtn.addEventListener('click', toggleModal);
+	}
 
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+	refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-  console.log(refs);
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden');
+	}
 
-  refs.form.addEventListener('submit', async e => {
-    e.preventDefault();
+	refs.form.addEventListener('submit', async e => {
+		e.preventDefault();
 
-    const form = new FormData(e.target);
+		const form = new FormData(e.target);
 
-    const data = Object.fromEntries(form);
+		const data = Object.fromEntries(form);
 
-    console.log(Object.fromEntries(form));
+		console.log(Object.fromEntries(form));
 
-    const phone = `+380${Inputmask.unmask(data.phone, {
-      mask: '+380(99) 999-99-99',
-    })}`;
+		const phone = `+380${Inputmask.unmask(data.phone, {
+			mask: '+380(99) 999-99-99',
+		})}`;
 
-    try {
-      const responseData = await makeOrder(
-        data.name,
-        phone,
-        data.email,
-        data.comment
-      );
-      toggleModal(refs);
-    } catch (error) {
-      Notiflix.Notify.failure(error.response.data.message);
-    }
+		try {
+			const responseData = await makeOrder(data.name, phone, data.email, data.comment);
+			toggleModal(refs);
+		} catch (error) {
+			Notiflix.Notify.failure(error.response.data.message);
+		}
 
-    console.log(responseData);
-  });
+		console.log(responseData);
+	});
 })();
