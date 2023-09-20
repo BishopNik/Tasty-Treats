db95b3ec9276effdd1920223ec94cb24d68ffc4f
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-09-20 19:16:32 +0200

    updateThemeByTime

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,53 +1,42 @@
 /** @format */
 
 function theme() {
-  const checkbox = document.querySelector(
-    '.theme-switcher input[type="checkbox"]'
-  );
+	const checkbox = document.querySelector('.theme-switcher input[type="checkbox"]');
 
-  const el = document.documentElement;
-  checkbox.addEventListener('change', () => {
-    if (el.hasAttribute('data-theme')) {
-      el.removeAttribute('data-theme');
-      localStorage.removeItem('theme');
-    } else {
-      el.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-    }
-  });
-  function setThemeFromMediaQuery() {
-    if (
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      el.setAttribute('data-theme', 'dark');
-      checkbox.checked = true;
-    }
-  }
+	const el = document.documentElement;
+	checkbox.addEventListener('change', () => {
+		if (el.hasAttribute('data-theme')) {
+			el.removeAttribute('data-theme');
+			localStorage.removeItem('theme');
+		} else {
+			el.setAttribute('data-theme', 'dark');
+			localStorage.setItem('theme', 'dark');
+		}
+	});
+	function setThemeFromMediaQuery() {
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			el.setAttribute('data-theme', 'dark');
+			checkbox.checked = true;
+		}
+	}
 
-  if (localStorage.getItem('theme') !== null) {
-    el.setAttribute('data-theme', 'dark');
-    checkbox.checked = true;
-  } else {
-    setThemeFromMediaQuery();
-    updateThemeByTime();
-  }
+	if (localStorage.getItem('theme') !== null) {
+		el.setAttribute('data-theme', 'dark');
+		checkbox.checked = true;
+	} else {
+		setThemeFromMediaQuery();
+	}
 }
 theme();
 
-const currentPageName = document
-    .querySelector('[meta-page-name]')
-    .getAttribute('meta-page-name');
-const navigationLinksArray = Array.from(
-    document.querySelectorAll('.nav-link')
-);
+const currentPageName = document.querySelector('[meta-page-name]').getAttribute('meta-page-name');
+const navigationLinksArray = Array.from(document.querySelectorAll('.nav-link'));
 
 navigationLinksArray.forEach(linkElement => {
-    const name = linkElement.getAttribute('link-name');
-    if (name === currentPageName) {
-        linkElement.classList.add('current');
-    } else {
-        linkElement.classList.remove('current');
-    }
+	const name = linkElement.getAttribute('link-name');
+	if (name === currentPageName) {
+		linkElement.classList.add('current');
+	} else {
+		linkElement.classList.remove('current');
+	}
 });
-
