76a0f41d803ed80fa4eaef584b5ac742d3a7fc41
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-09-21 00:20:52 +0200

    додала закриття на кнопку ескейп.поправила стилі

--- src/js/rating.js
+++ src/js/rating.js
@@ -3,12 +3,80 @@
 import { updateRating } from './fetch-api';
 import Notiflix from 'notiflix';
 
-function toggleModal(refs) {
-  refs.modal.classList.toggle('is-hidden');
+let refs = {};
+
+function toggleModal() {
+  const classList = refs.modal.classList;
+
+  if (classList.contains('is-hidden')) {
+    classList.remove('is-hidden');
+  } else {
+    classList.add('is-hidden');
+    refs.form.reset();
+    refs.span.innerHTML = '0.0';
+
+    refs.inputs.forEach(el => {
+      el.children[0].classList.remove('active');
+    });
+  }
+}
+
+const handler = e => {
+  const value = Number(e.target.value);
+
+  refs.span.innerHTML = value.toFixed(1);
+
+  refs.inputs.forEach((el, index) => {
+    console.log(el.classList);
+
+    if (index <= value - 1) {
+      el.children[0].classList.add('active');
+    } else {
+      el.children[0].classList.remove('active');
+    }
+  });
+};
+
+function onCloseModal({ target, key }) {
+  if (refs.modal === target || key === 'Escape') {
+    refs.modal.classList.add('is-hidden');
+  }
+}
+
+const handleSubmit = async (e, id) => {
+  e.preventDefault();
+
+  const form = new FormData(e.target);
+  const data = Object.fromEntries(form);
+
+  try {
+    const responseData = await updateRating(id, Number(data.rate), data.email);
+    toggleModal(refs);
+    refs.form.reset();
+
+    refs.inputs.forEach((el, index) => {
+      el.children[0].classList.remove('active');
+    });
+    refs.span.innerHTML = '0.0';
+  } catch (error) {
+    Notiflix.Notify.failure(error.response.data.message);
+  }
+};
+
+function removeListeners() {
+  refs.openModalBtn.removeListeners('click', toggleModal);
+  refs.closeModalBtn.removeListeners('click', toggleModal);
+  document.removeListeners('keydown', onCloseModal);
+
+  refs.inputs.forEach(el => {
+    el.children[1].removeListeners('change', handler);
+  });
+
+  refs.form.removeListeners('submit', handleSubmit);
 }
 
 export function createListeners(id) {
-  const refs = {
+  refs = {
     openModalBtn: document.querySelector('#modal-rating-opener'),
     closeModalBtn: document.querySelector('#modal-rating-closer'),
     modal: document.querySelector('#modal-rating-js'),
@@ -18,54 +86,19 @@ export function createListeners(id) {
     span: document.querySelector('.count-rating'),
   };
 
-  refs.openModalBtn.addEventListener('click', () => toggleModal(refs));
-  refs.closeModalBtn.addEventListener('click', () => toggleModal(refs));
+  try {
+    removeListeners();
+  } catch (e) {
+    console.log(e);
+  }
 
-  const handler = e => {
-    const value = Number(e.target.value);
-
-    refs.span.innerHTML = value.toFixed(1);
-
-    refs.inputs.forEach((el, index) => {
-      console.log(el.classList);
-
-      if (index <= value - 1) {
-        el.children[0].classList.add('active');
-      } else {
-        el.children[0].classList.remove('active');
-      }
-    });
-
-    console.log(value);
-  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  document.addEventListener('keydown', onCloseModal);
 
   refs.inputs.forEach(el => {
     el.children[1].addEventListener('change', handler);
   });
 
-  refs.form.addEventListener('submit', async e => {
-    e.preventDefault();
-
-    const form = new FormData(e.target);
-    const data = Object.fromEntries(form);
-
-    try {
-      const responseData = await updateRating(
-        id,
-        Number(data.rate),
-        data.email
-      );
-      toggleModal(refs);
-      refs.form.reset();
-
-      refs.inputs.forEach((el, index) => {
-        el.children[0].classList.remove('active');
-      });
-      refs.span.innerHTML = '0.0';
-    } catch (error) {
-      Notiflix.Notify.failure(error.response.data.message);
-    }
-
-    console.log(responseData);
-  });
+  refs.form.addEventListener('submit', async e => handleSubmit(e, id));
 }
